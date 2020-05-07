647406a8e6f7670c1afdf9ec21782210fb0f8478
Jehdi <46204336+jaizon2000@users.noreply.github.com>
2020-05-07 02:30:51 -0600

    made isclickable(link) function

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 "use strict";
 
+function isclickable(link) {
+    if (typeof link === 'undefined') {
+        return false;
+    } else {
+        return true;
+    }
+}
 $(function () {
     // Add Classes to Headers (Buttons)
     $("button.w3-btn").addClass("w3-block w3-left-align w3-blue-gray w3-xlarge");
@@ -9,18 +16,25 @@ $(function () {
 
     // EVENTS
     $("li").on({
+
         /* Highlight hover */
         mouseenter: function () {
-            $(this).addClass("w3-light-gray pointer");
+            var link = $(this).attr('id');
+            if (isclickable(link)) {
+                $(this).addClass("w3-light-gray pointer");
+            }
         },
         mouseleave: function () {
-            $(this).removeClass("w3-light-gray pointer");
+            var link = $(this).attr('id');
+            if (isclickable(link)) {
+                $(this).removeClass("w3-light-gray pointer");
+            }
         },
         /* ---------------- */
         // Opens link of curr clicked link
         click: function () {
             var link = $(this).attr('id');
-            if (typeof link !== 'undefined') {
+            if (isclickable(link)) {
                 var url = "https://" + link;
                 console.log(link);
                 window.open(url);
